353de60f93c2ab8bf78b52b1eb1994621bc5d8c9
Asus_Station <zhd.dmitry@gmail.com>
2024-02-08 21:30:42 +0200

    finished ex.13.3

--- 13/g_13_ex3_fuel.py
+++ 13/g_13_ex3_fuel.py
@@ -13,16 +13,22 @@ class Fuel:
 
         # Create Label field for gas, milage, MGP + empty field for result
         self.gas_label = tk.Label(self.gas_frame, text='Gas you\'ve waisted')
+        self.gas_entry = tk.Entry(self.gas_frame, width=10)
         self.milage_label = tk.Label(self.milage_frame, text='Mileage you can go')
+        self.milage_entry = tk.Entry(self.milage_frame, width=10)
 
+        self.value = tk.StringVar()
         self.MGP_label = tk.Label(self.MGP_frame, text='Calculated MGP')
+        self.MGP_result = tk.Label(self.MGP_frame, textvariable=self.value)
 
-        # Create buttons
-        self.calculate_button = tk.Button(self.button_frame, text='Calcualate MGP')
+        # Create buttons calculation and quit
+        self.calculate_button = tk.Button(self.button_frame, text='Calcualate MGP', command=self.__calc_MGP)
         self.quit_button = tk.Button(self.button_frame, text='Quit', command=self.main_window.destroy)
 
         self.gas_label.pack(side='left')
+        self.gas_entry.pack(side='left')
         self.milage_label.pack(side='left')
+        self.milage_entry.pack(side='left')
         self.MGP_label.pack(side='left')
 
         self.calculate_button.pack(side='left')
@@ -31,9 +37,16 @@ class Fuel:
         self.gas_frame.pack()
         self.milage_frame.pack()
         self.MGP_frame.pack()
+        self.MGP_result.pack()
         self.button_frame.pack()
 
         tk.mainloop()
+
+    def __calc_MGP(self):
+        self.gas_val = float(self.gas_entry.get())
+        self.milage_val = float(self.milage_entry.get())
+
+        self.value.set(f'{self.milage_val / self.gas_val:.02f}')
 
 
 if __name__ == '__main__':
